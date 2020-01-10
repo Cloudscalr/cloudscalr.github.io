31452814164fcffe7a227e0b17785bd82988768e
William Fleming <wfleming@hashicorp.com>
2020-01-09 17:34:40 -0700

    Website Commit

---
+++ js/cloudscalr.js
@@ -0,0 +1,64 @@
+/*
+  Author: Cloudscalr
+  Version: 1.0
+  URL: https://www.cloudscalr.com
+*/
+
+(function($) {
+  "use strict";
+
+  $(document).ready(function (){
+    'use strict';
+    
+    // Ajax mailchimp
+    $('#subscribe').ajaxChimp({
+      language: 'cs',
+      url: 'https://cloudscalr.us7.list-manage.com/subscribe/post?u=2321b920ee84f649fc89ae3e4&amp;id=67fec0bfb4'
+    });
+
+    // Mailchimp translation
+    //
+    // Defaults:
+    //'submit': 'Submitting...',
+    //  0: 'We have sent you a confirmation email',
+    //  1: 'Please enter a value',
+    //  2: 'An email address must contain a single @',
+    //  3: 'The domain portion of the email address is invalid (the portion after the @: )',
+    //  4: 'The username portion of the email address is invalid (the portion before the @: )',
+    //  5: 'This email address looks fake or invalid. Please enter a real email address'
+
+    $.ajaxChimp.translations.cs = {
+      'submit': 'Submitting...',
+      0: '<i class="fa fa-check"></i> We will be in touch soon!',
+      1: '<i class="fa fa-warning"></i> You must enter a valid e-mail address.',
+      2: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+      3: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+      4: '<i class="fa fa-warning"></i> E-mail address is not valid.',
+      5: '<i class="fa fa-warning"></i> E-mail address is not valid.'
+    }
+
+    // backstretch
+    //$("header").backstretch("img/bg.jpg");
+
+    // yt controls
+    /*
+    $('#yt-play').click(function(event){
+      event.preventDefault();
+      if ($(this).hasClass("fa-play") ) {
+          $('.yt-player').playYTP();
+      } else {
+          $('.yt-player').pauseYTP(); 
+      }
+      $(this).toggleClass("fa-play fa-pause");
+      return false;
+    });
+    $('#yt-volume').click(function(event){
+      event.preventDefault();
+      $('.yt-player').toggleVolume();
+      $(this).toggleClass("fa-volume-off fa-volume-up");
+      return false;
+    });
+    */
+  });
+
+})(jQuery);
